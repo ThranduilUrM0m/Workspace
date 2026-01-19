3b2bda7d96ca1249c937287af240e94a21f73098
ThranduilUrM0m <zakariaeboutaleb@gmail.com>
2026-01-20 00:07:32 +0100

    fix: correct compression imports for ESM compatibility

--- projects/Maxconfort/server/src/main.ts
+++ projects/Maxconfort/server/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import compression from 'compression';
+import * as compression from 'compression';
 import helmet from 'helmet';
 
 async function bootstrap(): Promise<void> {
